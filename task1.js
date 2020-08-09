43eea820cd65e8098e7375a3d0d4111394febe25
Takisha777 <55092730+Takisha777@users.noreply.github.com>
2020-08-09 21:32:06 +0300

    hw5

---
+++ task1.js
@@ -0,0 +1,15 @@
+// Задача 1:
+// Написать функцию заполнения двумерного массива. Имя произвольное.
+function arrUser(n,m) {
+    let randomNumber, arr = [];
+    for ( let i = 0; i < n; i++ ){
+        arr[i] = [];
+        for ( let j = 0; j < m; j++){
+            randomNumber = Math.trunc(Math.random()*100);
+            arr[i].push(randomNumber);
+        }
+        document.writeln(arr[i] + "<br>");
+    }
+
+}
+arrUser(10,10);
