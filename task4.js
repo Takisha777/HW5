a85e79b8b5cf3b99bfb86bb50e8b38f81371626d
Takisha777 <55092730+takisha777@users.noreply.github.com>
2020-08-10 15:38:26 +0300

    15.38

--- task4.js
+++ task4.js
@@ -1,12 +1,14 @@
 // Задача 4:
 // Создать функцию, которая убирает из строки все символы, которые мы передали вторым аргументом.
 // func("hello world", ['l', 'd']) // вернет нам "heo wor"
-debugger;
+
 function delSym(text, arr) {
-        // varText = toString(text);
         for (let i = 0; i < arr.length; i++){
-            text = text.replace(arr[i],'');
+            let find = arr[i];
+            let re = new RegExp(find,'g');
+            text = text.replace(re,'');
         }
+        return text;
 }
 
 document.writeln(delSym("hello world", ['l', 'd']));
