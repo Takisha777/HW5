56853aa9779a93fd81fa7cadeb31134440540990
Takisha777 <55092730+takisha777@users.noreply.github.com>
2020-08-11 08:41:05 +0300

    8.40

--- task5.js
+++ task5.js
@@ -5,13 +5,18 @@
 // function isEven(x) { return x % 2 == 0; } // проверяет на четность
 // console.log(filter(input, isEven)); // [2, 4, 6]
 
-function filter(isEven, arr) {
+let input = [1, 2, 3, 4, 5, 6];
+function isEven(x) {
+    return x % 2 === 0;
+}
+function filter(arr, isEven) {
     let arrNew =[];
     for (let i = 0; i < arr.length; i++){
-        arrNew.push(arr[i]);
+        if (isEven(arr[i])){
+            arrNew.push(arr[i]);
+        }
     }
     return arrNew;
 }
-function isEven(x) {
-    return x % 2 == 0;
-}
+
+console.log(filter(input, isEven));
